8607ba112ab9359c91cf0efaccd8e85369743310
SheikhhHassan <brravo08@gmail.com>
2025-03-02 14:51:27 +0500

    Python files added

---
+++ python.py
@@ -0,0 +1,12 @@
+class Calculator():
+    def __init__(self):
+        pass
+
+    def sum(self, val1, val2):
+        return val1 + val2
+
+    def subtract(self, val1, val2):
+        return val1 - val2
+    
+calculator = Calculator()
+print(calculator.sum(12, 15))    
